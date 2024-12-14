cacc8f2bfe7c817b24066da0aaa56ff18648ee30
arthur-teixeira <arthurcarvalhot@yahoo.com.br>
2024-12-14 15:54:09 -0300

    Day 14 parts 1 and 2

--- src/days/day14.rs
+++ src/days/day14.rs
@@ -1,3 +1,9 @@
+use std::{
+    fs::{self, File, OpenOptions},
+    io::Write,
+    os::unix::fs::FileExt,
+};
+
 use itertools::Itertools;
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
@@ -30,29 +36,14 @@ impl Robot {
     }
 
     fn walk(&mut self, times: isize, grid_bounds: (isize, isize)) {
-        for _ in 0..times {
-            let (x, y) = self.pos;
-            let (dx, dy) = self.v;
-            let (grid_x, grid_y) = grid_bounds;
+        let (x, y) = self.pos;
+        let (dx, dy) = self.v;
+        let (grid_x, grid_y) = grid_bounds;
 
-            let dx = dx % grid_x;
-            let dy = dy % grid_y;
+        let new_x = (x + dx * times).rem_euclid(grid_x + 1);
+        let new_y = (y + dy * times).rem_euclid(grid_y + 1);
 
-            let mut new_x = x + dx;
-            if new_x < 0 {
-                new_x = new_x + grid_x + 1;
-            } else if new_x > grid_x {
-                new_x = (new_x % grid_x) - 1;
-            }
-
-            let mut new_y = y + dy;
-            if new_y < 0 {
-                new_y = new_y + grid_y + 1;
-            } else if new_y > grid_y {
-                new_y = (new_y % grid_y) - 1;
-            }
-            self.pos = (new_x, new_y);
-        }
+        self.pos = (new_x, new_y);
     }
 
     fn get_quadrant(
@@ -65,10 +56,6 @@ impl Robot {
         let divider_x = grid_x / 2;
         let divider_y = grid_y / 2;
 
-        // q1, x < div && y < div
-        // q2, x < div && y > div
-        // q3, x > div && y < div
-        // q4, x > div && y > div
         if x < divider_x && y < divider_y {
             counter.0 += 1;
         } else if x < divider_x && y > divider_y {
@@ -82,53 +69,136 @@ impl Robot {
 }
 
 pub fn day14(input: String) {
-    // let grid_bounds = (101, 103);
-    let grid_bounds = (10, 6);
-    let bots = input
+    println!("Part 1: {}", part1(&input));
+    part2(input);
+}
+
+fn part1(input: &str) -> usize {
+    let grid_bounds = (100, 102);
+    let quadrants = input
         .lines()
         .map(|line| {
             let mut r = Robot::from_line(line);
             r.walk(100, grid_bounds);
             r
         })
+        .fold((0, 0, 0, 0), |mut acc, cur| {
+            cur.get_quadrant(&mut acc, grid_bounds);
+            acc
+        });
+
+    quadrants.0 * quadrants.1 * quadrants.2 * quadrants.3
+}
+
+fn part2(input: String) {
+    let grid_bounds = (100, 102);
+    let bots = input
+        .lines()
+        .map(|line| Robot::from_line(line))
+        .collect_vec();
+
+    let mut x_variances = vec![];
+    let mut y_variances = vec![];
+    for i in 0..103 {
+        let mut bs = bots.clone();
+
+        let bs = bs
+            .iter_mut()
+            .map(|b| {
+                b.walk(i + 1, grid_bounds);
+                b.to_owned()
+            })
+            .collect_vec();
+
+        let x_var = x_variance(&bs);
+        let y_var = y_variance(&bs);
+        x_variances.push((i + 1, x_var));
+        y_variances.push((i + 1, y_var));
+    }
+
+    let mut bx = 0;
+    let mut min_var_x = usize::MAX;
+    for (sec, var) in &x_variances {
+        if *var < min_var_x {
+            bx = *sec;
+            min_var_x = *var;
+        }
+    }
+
+    let mut by = 0;
+    let mut min_var_y = usize::MAX;
+    for (sec, var) in &y_variances {
+        if *var < min_var_y {
+            by = *sec;
+            min_var_y = *var;
+        }
+    }
+
+    let (h, w) = (grid_bounds.1 + 1, grid_bounds.0 + 1);
+    let t = bx + (((51 * (by - bx)).rem_euclid(h)) * w);
+
+
+    let mut bots = bots.clone();
+    bots.iter_mut()
+        .map(|b| {
+            b.walk(t, grid_bounds);
+            b.to_owned()
+        })
         .collect_vec();
 
     draw(&bots, grid_bounds);
-    let quadrants = bots.iter().fold((0, 0, 0, 0), |mut acc, cur| {
-        cur.get_quadrant(&mut acc, grid_bounds);
-        acc
-    });
+    println!("Best time is {t}");
+}
 
-    let safety_factor = quadrants.0 * quadrants.1 * quadrants.2 * quadrants.3;
-    println!("Quadrants: {:?}", quadrants);
-    println!("Part 1: {safety_factor}");
+fn x_variance(bots: &Vec<Robot>) -> usize {
+    let n = bots.len();
+    let xs = bots.iter().map(|b| b.pos.0 as usize).collect_vec();
+    let avg = xs.iter().sum::<usize>() / n;
+
+    let mut var = 0;
+    for i in 0..n {
+        var += avg.abs_diff(xs[i]).pow(2);
+    }
+
+    var /= n;
+
+    var
+}
+
+fn y_variance(bots: &Vec<Robot>) -> usize {
+    let n = bots.len();
+    let ys = bots.iter().map(|b| b.pos.1 as usize).collect_vec();
+    let avg = ys.iter().sum::<usize>() / n;
+
+    let mut var = 0;
+    for i in 0..n {
+        var += avg.abs_diff(ys[i]).pow(2);
+    }
+
+    var /= n;
+    var
 }
 
 fn draw(bots: &Vec<Robot>, grid_bounds: (isize, isize)) {
-    let (x, y) = grid_bounds;
-    let divider_x = x / 2;
-    let divider_y = y / 2;
+    let mut buf = String::new();
 
+    let (x, y) = grid_bounds;
     for j in 0..=y {
         for i in 0..=x {
-            if i == divider_x {
-                print!(" ");
-                continue;
-            }
-            if j == divider_y {
-                print!(" ");
-                continue;
-            }
             let in_pos = bots.iter().filter(|b| b.pos == (i, j)).count();
             if in_pos > 0 {
-                print!("{in_pos}");
+                buf.push('*');
             } else {
-                print!(".");
+                buf.push(' ');
             }
         }
 
-        println!();
+        buf.push('\n');
     }
+
+    // clear
+    print!("\x1B[2J");
+    println!("{buf}");
 }
 
 #[cfg(test)]
