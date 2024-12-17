849a5333a62fabce56c203155f810eeb1cd8524e
arthur-teixeira <arthurcarvalhot@yahoo.com.br>
2024-12-17 16:48:13 -0300

    Day 15 part 2

--- src/days/day15.rs
+++ src/days/day15.rs
@@ -1,4 +1,8 @@
-use std::fmt::Display;
+use std::{
+    fmt::{write, Display},
+    thread::sleep,
+    time::Duration,
+};
 
 use itertools::Itertools;
 
@@ -8,6 +12,8 @@ enum Kind {
     Box,
     Wall,
     Empty,
+    BoxLeft,
+    BoxRight,
 }
 
 impl Kind {
@@ -25,21 +31,35 @@ impl Kind {
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 struct Object {
     kind: Kind,
-    movable: bool,
 }
 
 impl Object {
+    fn movable(&self) -> bool {
+        match self.kind {
+            Kind::Wall => false,
+            _ => true,
+        }
+    }
     fn new(c: char) -> Self {
         let kind = Kind::from_char(c);
 
-        Self {
-            movable: match kind {
-                Kind::Robot => true,
-                Kind::Box => true,
-                Kind::Empty => true,
-                Kind::Wall => false,
-            },
-            kind,
+        Self { kind }
+    }
+
+    fn new_part2(c: char) -> Vec<Self> {
+        match c {
+            '@' => vec![Self { kind: Kind::Robot }, Self { kind: Kind::Empty }],
+            'O' => vec![
+                Self {
+                    kind: Kind::BoxLeft,
+                },
+                Self {
+                    kind: Kind::BoxRight,
+                },
+            ],
+            '#' => vec![Self { kind: Kind::Wall }, Self { kind: Kind::Wall }],
+            '.' => vec![Self { kind: Kind::Empty }, Self { kind: Kind::Empty }],
+            _ => unreachable!(),
         }
     }
 }
@@ -89,11 +109,10 @@ impl Grid {
             .and_then(|obj| Some(*obj))
     }
 
-    fn do_move(&mut self, pos: (isize, isize), m: &Move) -> bool {
+    fn can_move(&self, pos: (isize, isize), m: &Move, checking_side: bool) -> bool {
         let (x, y) = pos;
         let Move(dx, dy) = *m;
-        let new_pos = (x + dx, y + dy);
-        let (new_x, new_y) = new_pos;
+
         let obj = self
             .at(x, y)
             .expect("Expected position to be within bounds");
@@ -101,28 +120,73 @@ impl Grid {
         if obj.kind == Kind::Empty {
             return true;
         }
-        if obj.movable {
-            let moved = self.do_move(new_pos, m);
-            if moved {
-                self.values[new_x as usize][new_y as usize] = self.values[x as usize][y as usize];
-                self.values[x as usize][y as usize] = Object {
-                    movable: true,
-                    kind: Kind::Empty,
-                };
-
-                match obj.kind {
-                    Kind::Robot => {
-                        self.bot_position = new_pos;
-                    }
-                    _ => {}
-                };
-                return true;
-            } else {
-                return false;
-            }
+        if obj.kind == Kind::Wall {
+            return false;
         }
 
-        return false;
+        match obj.kind {
+            Kind::Empty => true,
+            Kind::BoxLeft if !checking_side => {
+                assert!(self.at(x, y + 1).unwrap().kind == Kind::BoxRight);
+                if dx == 0 && dy == 1 {
+                    obj.movable() && self.can_move((x, y + 1), m, true)
+                } else {
+                    obj.movable()
+                        && self.can_move((x + dx, y + dy), m, false)
+                        && self.can_move((x, y + 1), m, true)
+                }
+            }
+            Kind::BoxRight if !checking_side => {
+                assert!(self.at(x, y - 1).unwrap().kind == Kind::BoxLeft);
+                if dx == 0 && dy == -1 {
+                    obj.movable() && self.can_move((x, y - 1), m, true)
+                } else {
+                    obj.movable()
+                        && self.can_move((x + dx, y + dy), m, false)
+                        && self.can_move((x, y - 1), m, true)
+                }
+            }
+            _ => obj.movable() && self.can_move((x + dx, y + dy), m, false),
+        }
+    }
+
+    fn do_move(&mut self, pos: (isize, isize), m: &Move, moving_side: bool) {
+        let (x, y) = pos;
+        let Move(dx, dy) = *m;
+        let obj = self
+            .at(x, y)
+            .expect("Expected position to be within bounds");
+
+        if obj.kind == Kind::Empty {
+            return;
+        }
+
+        if obj.kind == Kind::BoxLeft && !moving_side {
+            let rhs = self.at(x, y + 1).expect("Expected box right hand side");
+            assert!(rhs.kind == Kind::BoxRight);
+            self.do_move((x, y + 1), m, true);
+        }
+
+        if obj.kind == Kind::BoxRight && !moving_side {
+            let lhs = self.at(x, y - 1).expect("Expected box left hand side");
+            assert!(lhs.kind == Kind::BoxLeft);
+            self.do_move((x, y - 1), m, true);
+        }
+
+        if !obj.movable() {
+            return;
+        }
+
+        let new_pos = (x + dx, y + dy);
+        let (new_x, new_y) = new_pos;
+        self.do_move(new_pos, m, false);
+
+        self.values[new_x as usize][new_y as usize] = self.values[x as usize][y as usize];
+        self.values[x as usize][y as usize] = Object { kind: Kind::Empty };
+
+        if obj.kind == Kind::Robot {
+            self.bot_position = new_pos;
+        }
     }
 
     fn draw(&self) {
@@ -134,6 +198,8 @@ impl Grid {
                     Kind::Wall => '#',
                     Kind::Robot => '@',
                     Kind::Empty => '.',
+                    Kind::BoxLeft => '[',
+                    Kind::BoxRight => ']',
                 };
 
                 print!("{elem}");
@@ -148,6 +214,7 @@ impl Grid {
             for y in 0..self.cols {
                 acc += match self.at(x, y).expect("Expected to be within bounds").kind {
                     Kind::Box => (100 * x) + y,
+                    Kind::BoxLeft => (100 * x) + y,
                     _ => 0,
                 };
             }
@@ -158,28 +225,38 @@ impl Grid {
 }
 
 pub fn day15(input: String) {
-    let (mut grid, moves) = parse(&input);
-    println!("Part 1: {}", part1(&mut grid, moves));
+    println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
 }
 
-fn parse(input: &str) -> (Grid, Vec<Move>) {
+fn parse(input: &str, part: u8) -> (Grid, Vec<Move>) {
     let mut input = input.split("\n\n");
-    let mat = input
-        .next()
-        .unwrap()
-        .lines()
-        .map(|line| line.chars().map(Object::new).collect_vec())
-        .collect_vec();
+    let mat;
+    if part == 1 {
+        mat = input
+            .next()
+            .unwrap()
+            .lines()
+            .map(|line| line.chars().map(Object::new).collect_vec())
+            .collect_vec();
+    } else {
+        mat = input
+            .next()
+            .unwrap()
+            .lines()
+            .map(|line| line.chars().map(Object::new_part2).flatten().collect_vec())
+            .collect_vec();
+    }
 
     let rows = mat.len() as isize;
     let cols = mat[0].len() as isize;
 
-    let bot_y = mat
+    let bot_x = mat
         .iter()
         .position(|row| row.iter().position(|o| o.kind == Kind::Robot).is_some())
         .expect("Expected guard to be in the map");
 
-    let bot_x = mat[bot_y]
+    let bot_y = mat[bot_x]
         .iter()
         .position(|o| o.kind == Kind::Robot)
         .expect("Expected guard to be in the map");
@@ -202,10 +279,23 @@ fn parse(input: &str) -> (Grid, Vec<Move>) {
     (g, moves)
 }
 
-fn part1(grid: &mut Grid, moves: Vec<Move>) -> isize {
+fn part1(input: &str) -> isize {
+    let (mut grid, moves) = parse(&input, 1);
+    solve(&mut grid, moves)
+}
+
+fn part2(input: &str) -> isize {
+    let (mut grid, moves) = parse(&input, 2);
+    solve(&mut grid, moves)
+}
+
+fn solve(grid: &mut Grid, moves: Vec<Move>) -> isize {
     for m in moves {
-        grid.do_move(grid.bot_position, &m);
+        if grid.can_move(grid.bot_position, &m, false) {
+            grid.do_move(grid.bot_position, &m, false);
+        }
     }
 
     grid.get_gps_sum()
 }
+
